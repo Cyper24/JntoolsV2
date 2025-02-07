1f463e015ec0063b994ceb4512778fb614b9d316
Nicc <36534858+Cyper24@users.noreply.github.com>
2025-02-07 21:22:27 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,20 @@
 import streamlit as st
-
+import requests
 st.set_page_config(layout="wide",
                    initial_sidebar_state="auto",)
 
+url = "https://jmsgw.jntexpress.id/notice/web/getBannerList"
 with st.sidebar:
         ato = st.text_input("Input AuthToken","",key="ato")
+at=(st.session_state.ato).strip(" ")
+payload = ""
+headers = {
+    "cookie": "HWWAFSESID=a00e27f02785ef49ce5; HWWAFSESTIME=1738201375713",
+    "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/132.0.0.0 Safari/537.36 Edg/132.0.0.0",
+    "authtoken": f"{at}"
+}
+
+response = requests.request("GET", url, data=payload, headers=headers)
 
 pages = {
     "Dashboard": [
@@ -19,6 +29,8 @@ pages = {
         st.Page("reportout.py", title="Report Harian Out",icon=":material/menu:"),
     ],
 }
-if st.session_state.ato:
+if response.status_code == 200:
     pg = st.navigation(pages)
     pg.run()
+else:
+    st.caption("Enter Valid AuthToken")

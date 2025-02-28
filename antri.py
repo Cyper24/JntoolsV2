e7ab5804ab68e8802c595635355264b841f3a5be
Nicc <36534858+Cyper24@users.noreply.github.com>
2025-02-28 19:12:23 +0700

    Update antri.py

--- antri.py
+++ antri.py
@@ -3,12 +3,25 @@ import streamlit as st
 from datetime import datetime, timedelta
 import pandas as pd
 
+default_start, default_end = datetime.now().replace(hour=0, minute=0, second=0)- timedelta(days=1), datetime.now().replace(hour=23, minute=59, second=59)
+default_start = default_start.strftime('%Y-%m-%d %H:%M:%S')
+default_end =  default_end.strftime('%Y-%m-%d %H:%M:%S')
+
+
 at=(st.session_state.ato).strip(" ")
 st.title("Cek Antrian")
 
 list = []
 url = "https://jmsgw.jntexpress.id/transportation/tmsShipmentEvent/report"
-querystring = {"current":"1","size":"100","shipmentState":"4","tmsType":"1","arriveNetworkCode":"SOC999","timeType":"1","startTime":"2025-02-26 00:00:00","endTime":"2025-02-27 23:59:59"}
+querystring = {"current":"1",
+               "size":"100"
+               ,"shipmentState":"4"
+               ,"tmsType":"1",
+               "arriveNetworkCode":"SOC999"
+               ,"timeType":"1",
+               "startTime":default_start,
+               "endTime": default_end 
+               }
 headers = {
     "cookie": "HWWAFSESID=a00e27f02785ef49ce5; HWWAFSESTIME=1738201375713",
     "Content-Type": "application/json",
